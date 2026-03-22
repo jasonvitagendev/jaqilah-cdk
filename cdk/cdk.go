29d6ec06ae57aaf2e1b713cb896c35d70152ba2c
jason <jasonvitagen.developer@gmail.com>
2026-03-22 15:56:01 +0800

    Commit

--- cdk/cdk.go
+++ cdk/cdk.go
@@ -4,25 +4,10 @@ import (
 	"os"
 
 	"github.com/aws/aws-cdk-go/awscdk/v2"
-	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/jasonvitagendev/jaqilah-cdk/cdk/stacks"
 
 	"github.com/aws/jsii-runtime-go"
 )
-
-type USEast1CertificatesStackProps struct {
-	awscdk.StackProps
-}
-
-func NewUSEast1CertificatesStack(scope constructs.Construct, id string, props *USEast1CertificatesStackProps) awscdk.Stack {
-	var sprops awscdk.StackProps
-	if props != nil {
-		sprops = props.StackProps
-	}
-	stack := awscdk.NewStack(scope, &id, &sprops)
-
-	return stack
-}
 
 func main() {
 	defer jsii.Close()
